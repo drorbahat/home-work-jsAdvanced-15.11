38dff2c6cc0df02a0d450e87498cee67c6ad7e5a
dror bahat <bahat.dror@gmail.com>
2021-11-17 17:58:13 +0200

    finished exercise 2

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 const outputField = document.getElementById("output-field")
-let counter = 0
 
 const runCode = () => {
     outputField.innerHTML = ''
-    let input = document.getElementById("code-box").value
-    doWork(checkForNum, input)
+    doWork(showName)
 }
 
-const checkForNum = (input) => {
-    try {
-        if (isNaN(input)) {
-            outputField.innerHTML = "לא מספר!"
-        } else {
-            outputField.innerHTML = input
-        }
-    } catch (e) {
-    }
-
+const showName = () => {
+    outputField.innerHTML = "Dror Bahat"
 }
 
-const doWork = (callBack, input) => {
-    callBack(input)
+const doWork = (callBack) => {
+    callBack()
 }
